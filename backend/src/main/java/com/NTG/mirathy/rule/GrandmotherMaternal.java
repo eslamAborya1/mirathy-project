38ff5d8da9944e81d78c7cfc2645e042548f8537
Noureldin Elbasuni <Noureldin.elbasuni@gmail.com>
2026-01-22 22:55:13 +0200

    adding Grandmother paternal and grandmother matternal

---
+++ backend/src/main/java/com/NTG/mirathy/rule/GrandmotherMaternal.java
@@ -0,0 +1,43 @@
+package com.NTG.mirathy.rule;
+
+import com.NTG.mirathy.DTOs.InheritanceShareDto;
+import com.NTG.mirathy.Entity.Enum.FixedShare;
+import com.NTG.mirathy.Entity.Enum.HeirType;
+import com.NTG.mirathy.Entity.Enum.ShareType;
+import com.NTG.mirathy.Entity.Enum.TaaasibRule;
+import com.NTG.mirathy.util.InheritanceCase;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrandmotherMaternal implements InheritanceRule{
+    @Override
+    public boolean canApply(InheritanceCase c) {
+        return c.has(HeirType.GRANDMOTHER_MATERNAL);
+    }
+
+    @Override
+    public InheritanceShareDto calculate(InheritanceCase c) {
+        HeirType heirType = HeirType.GRANDMOTHER_MATERNAL;
+        String reason = null;
+        FixedShare fixedShare = null;
+        ShareType shareType = null;
+        TaaasibRule taaasibRule = null;
+
+         if (c.has(HeirType.MOTHER)){
+            shareType=ShareType.Mahgub;
+            reason="الأم تحجب جميع الجدات سواء من جهتها أو من جهة الأب لأن الجدات يرثن بصفة الأمومة المجازية ولا يمكن إعمال المجاز فى وجود الواقع.";
+        }else if (c.has(HeirType.GRANDMOTHER_PATERNAL)){
+            shareType=ShareType.FIXED;
+            fixedShare=FixedShare.TWELVE;
+            reason="فرض الجدة الصحيحة هو السدس .وتشتركن فيه الجدات الصحيحات المتحدات فى الدرجة ،أو جدة بعيدة من جهة الأم مع قريبة من جهة الأب (عند الشافعيه والمالكية واختار رأيهما القانون الجزائرى و المغربى والإماراتى). لما روى عن النبى ﷺ أنه أطعم السدس ثلاث جدات.والجدة الصحيحة هى أم الأم ومن علاها بمحض الإناث وأم الأب ومن علاها بمحض الذكور.والجدة الفاسدة هى من تدلى بذكر غير وارث مثل أم أب أم الميت فهى من ذوى الأرحام.";
+        }else {
+            shareType=ShareType.FIXED;
+            fixedShare=FixedShare.SIXTH;
+            reason="فرض الجدة الصحيحة هو السدس .وتشتركن فيه الجدات الصحيحات المتحدات فى الدرجة ،أو جدة بعيدة من جهة الأم مع قريبة من جهة الأب (عند الشافعيه والمالكية واختار رأيهما القانون الجزائرى و المغربى والإماراتى). لما روى عن النبى ﷺ أنه أطعم السدس ثلاث جدات.والجدة الصحيحة هى أم الأم ومن علاها بمحض الإناث وأم الأب ومن علاها بمحض الذكور.والجدة الفاسدة هى من تدلى بذكر غير وارث مثل أم أب أم الميت فهى من ذوى الأرحام.";
+        }
+
+
+        return new InheritanceShareDto(heirType, shareType, fixedShare, taaasibRule, reason);
+
+    }
+}
